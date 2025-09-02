6ca04982240ffbbc2aca5d97d51b020b53a29cb6
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-09-02 17:03:07 -0600

    Nav bar and routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app';
+import { HomeComponent } from './pages/home';
+import { AboutComponent } from './pages/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '**', redirectTo: '' }
+];
+
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes)
+  ]
+});
