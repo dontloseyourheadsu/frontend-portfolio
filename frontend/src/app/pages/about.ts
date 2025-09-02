7c37d9419868ab59907b47d6132d008792979f4c
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-09-02 17:03:13 -0600

    Sample pages

---
+++ frontend/src/app/pages/about.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  template: '<h1>Welcome to the About Page</h1>',
+  styles: ['h1 { font-family: Lato; color: #333; }']
+})
+export class AboutComponent {
+
+}
---
+++ frontend/src/app/pages/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+    template: '<h1>Welcome to the Home Page</h1>',
+  styles: ['h1 { font-family: Lato; color: #333; }']
+})
+export class HomeComponent {
+
+}
