b39903d70305dc6afa73e85c4b24f36fb1e925cb
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-09-05 13:04:15 -0600

    test main page colors

--- frontend/src/app/pages/home.ts
+++ frontend/src/app/pages/home.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-    template: '<h1>Welcome to the Home Page</h1>',
+    template: '<h1 [style.color]="\'var(--text-color)\'">Welcome to the Home Page</h1>',
   styles: ['h1 { font-family: Lato; color: #333; }']
 })
 export class HomeComponent {
