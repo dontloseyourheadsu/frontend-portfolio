17b6ed8ec9eb900e7bc47b6187ac20ee0e5a1ab6
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-09-13 21:35:09 -0600

    Nav items active style

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,17 +1,9 @@
-import { provideRouter, Routes } from '@angular/router';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home';
 import { AboutComponent } from './pages/about';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
-    { path: '**', redirectTo: '' }
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: '' }
 ];
-
-bootstrapApplication(App, {
-  providers: [
-    provideRouter(routes)
-  ]
-});
