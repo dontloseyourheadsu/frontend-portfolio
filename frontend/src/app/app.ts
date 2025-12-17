8748dc995c99da191c0004dcd78cf5f8da59c076
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-12-16 20:39:12 -0600

    handle snackbar service

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './nav-bar/nav-bar';
+import { SnackbarComponent } from './shared/snackbar/snackbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar],
+  imports: [RouterOutlet, NavBar, SnackbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
