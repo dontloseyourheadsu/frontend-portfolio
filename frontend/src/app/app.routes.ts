c1fdbc41a43c327716148c3339819e52ba50a465
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-12-19 13:56:12 -0600

    Projects page template routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home-component/home.component';
 import { AboutComponent } from './pages/about';
 import { ProjectsPage } from './pages/projects/projects-page/projects-page';
-import { LowLevelProjectsPage } from './pages/projects/low-level-projects-page/low-level-projects-page';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'projects', component: ProjectsPage },
-  { path: 'projects/low-level', component: LowLevelProjectsPage },
+  { path: 'projects/:id', component: ProjectsPage },
   { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '' }
 ];
