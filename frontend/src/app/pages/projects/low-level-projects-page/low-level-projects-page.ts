54a98333ba1ded2944ced9c13c6a3d73a242b32e
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-12-18 19:56:50 -0600

    Added projects and low level projects page

---
+++ frontend/src/app/pages/projects/low-level-projects-page/low-level-projects-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-low-level-projects-page',
+  imports: [],
+  templateUrl: './low-level-projects-page.html',
+  styleUrl: './low-level-projects-page.css'
+})
+export class LowLevelProjectsPage {
+
+}
